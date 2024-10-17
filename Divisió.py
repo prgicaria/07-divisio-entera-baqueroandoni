bc5a9db6c7a2e232bffae6d2c7a80e5ac4b06700
baqueroandoni <abaquero@instituticaria.cat>
2024-10-17 23:48:08 +0200

    Add files via upload

---
+++ Divisió.py
@@ -0,0 +1,7 @@
+D = int(input("Dividend: "))
+d = int(input("Divisor: "))
+resultat = D // d 
+residu = D-resultat*d
+print(f"Divisió: {D}/{d}")
+print(f"Quocient: {resultat}")
+print(f"Residu: {residu}")
